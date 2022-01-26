9fbb286db5ff8c8c663959cb785707b6a050be02
Syldriem <93327634+Syldriem@users.noreply.github.com>
2022-01-26 12:11:53 +0100

    datasource + transaction

---
+++ Transaction.py
@@ -0,0 +1,15 @@
+from curses import ALL_MOUSE_EVENTS
+from datetime import date
+from Datasource import Datasource
+
+class Transaction:
+
+    trans_id = 
+    customer_id =
+    account_id =
+    date =
+    amount =
+
+    def add_transaction(account_id, amount):
+
+
